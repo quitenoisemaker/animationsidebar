6c48c7812a19528f25504d42d3419a2b82f596e2
samson <samsonojugo@gmail.com>
2020-03-29 11:32:14 -1100

    darkmode toggle

---
+++ darkmode.js
@@ -0,0 +1,8 @@
+const checkbox =
+
+document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+	//change the theme of the website
+	document.body.classList.toggle('dark');
+} );
